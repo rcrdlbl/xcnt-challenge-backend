8ab4c88604ba9973bba6444e4199d038b93b5efe
rcrdlbl <bluh@outlook.com>
2019-10-04 17:10:35 -0400

    expense listener now saves to database

--- app/jobs/listen_for_expense_job.rb
+++ app/jobs/listen_for_expense_job.rb
@@ -8,7 +8,20 @@ class ListenForExpenseJob < ApplicationJob
 
     response = HTTP.get("https://cashcog.xcnt.io/stream")
     response.body.each do |piece|
-      puts piece.to_s
+      parsed = piece && piece.length >= 2 ? JSON.parse(piece) : nil
+      if parsed
+        expense = Expense.create(
+          uuid: parsed["uuid"],
+          description: parsed["description"],
+          created_at: parsed["created_at"],
+          amount: parsed["amount"],
+          currency: parsed["currency"],
+        )
+        expense.employee = Employee.find_or_create_by(uuid: parsed["employee"]["uuid"]) do |employee|
+          employee.first_name = parsed["employee"]["first_name"]
+          employee.last_name = parsed["employee"]["last_name"]
+        end
+      end
     end
   end
 end
