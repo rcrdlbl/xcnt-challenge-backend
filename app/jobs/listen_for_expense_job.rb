e2acca5b8aff19ff22259ecd44f62c8610fc2363
rcrdlbl <bluh@outlook.com>
2019-10-23 21:47:49 -0400

    added validation to expense listener

--- app/jobs/listen_for_expense_job.rb
+++ app/jobs/listen_for_expense_job.rb
@@ -2,14 +2,37 @@ class ListenForExpenseJob < ApplicationJob
   require 'http'
   queue_as :default
 
+  # def perform(*args)
+  #   # Do something later
+  #   puts "Expense Listener Started!"
+  #
+  #   response = HTTP.get("https://cashcog.xcnt.io/stream")
+  #   response.body.each do |piece|
+  #     parsed = piece && piece.length >= 2 && JSON.parse(piece) ? JSON.parse(piece) : nil
+  #     if parsed
+  #       expense = Expense.find_or_create_by(id: parsed["uuid"]) do |exp|
+  #         exp.description = parsed["description"]
+  #         exp.created_at = parsed["created_at"]
+  #         exp.amount = parsed["amount"]
+  #         exp.currency = parsed["currency"]
+  #       end
+  #       expense.employee = Employee.find_or_create_by(id: parsed["employee"]["uuid"]) do |employee|
+  #         employee.first_name = parsed["employee"]["first_name"]
+  #         employee.last_name = parsed["employee"]["last_name"]
+  #       end
+  #       expense.save
+  #     end
+  #   end
+  # end
+
   def perform(*args)
-    # Do something later
     puts "Expense Listener Started!"
 
     response = HTTP.get("https://cashcog.xcnt.io/stream")
     response.body.each do |piece|
-      parsed = piece && piece.length >= 2 && JSON.parse(piece) ? JSON.parse(piece) : nil
-      if parsed
+      piece = piece.gsub("\n", '').gsub(/}{/, '},{').prepend('[').concat(']')
+      parsed_pieces = piece && piece.length >= 2 && JSON.parse(piece) ? JSON.parse(piece) : nil
+      parsed_pieces.each do |parsed|
         expense = Expense.find_or_create_by(id: parsed["uuid"]) do |exp|
           exp.description = parsed["description"]
           exp.created_at = parsed["created_at"]
